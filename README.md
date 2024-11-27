c6589c36893fa7bcbc2bcc4f935f203761d8e1b0
jensbrak <52660827+jensbrak@users.noreply.github.com>
2024-11-27 10:12:40 +0100

    Stuff made long time ago that I forgot to commit
    Some changes from long time ago that was not committed.
    Plus .NET9, because why not since it "just" came out

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# Vide
+# Vide - ValheimItemDataExtract tool
+
+Tool to extract Valheim inventory item data from asset files. 
+
+Note: asset files has to be extracted from the game beforehand using another tool, e.g., AssetRipper.
+
+# Credits
+I didn't come up with the concept of this program at all. User [Brandon-T](https://github.com/Brandon-T) did, in a discussion [here](https://github.com/Wufflez/Loki/issues/30). I just got inspired and wrote my own version in C# and used it as an excuse to explore things related to this. It's somewhat modified to actually work as a real program and not the hack I started out to do.
+
+
+# How to use AssetRipper
+Steps required to extract asset files from Valheim game, using AssetRipper:
+
+1. Get a release (or the source code) from: https://github.com/AssetRipper/AssetRipper 
+1. Unpack (or build) AssetRipper and run it.
+1. Load Valheim game content into AssetRipper:
+	1. Select `File > Open Folder`.
+	1. Locate and select Valheim installation folder, i.e., `c:\Program Files (x86)\Steam\steamapps\common\Valheim\`.
+	1. Wait for AssetRipper to load game content.
+1. Export loaded game content from AssetRipper:
+	1. Select `Export > Export all Files`.
+	1. Select (create if needed) an export folder ***outside the Valheim installation folder***, e.g., `c:\temp\`.
+	1. Wait for AssetRipper to export game content.
+
+Nested within the export folder there should be a folder named `Assets` (e.g., `c:\temp\valheim\ExportedProject\Assets`). 
+*The path to this folder is what ValheimItemDataExtract expects as source.*
+
+# Implementation notes
+ValheimItemDataExtract is just a simple and very specialized text extractor:
+
+- Get localization data from file `localization.txt` within the `Resources\` subfolder.
+- Get item data from prefab files (`*.prefab`) within the `PrefabInstance\` subfolder.
+- Export item data to a CSV (Comma Separated Values) file, on item per line.
+  
+Assumptions made (that seems to work good enough):
+
+- The name of a prefab file (without .prefab extension) equals the name of the item it defines.
+	- Not true beginning with Valheim 0.218.15 (Ashlands). Item data for some items moved to a new file with suffix '_0'.
+	- For now, fix is just to remove the suffix. Probably should read name of item from within file instead.
+- If a prefab file has a section `m_itemData` in it, the data within that section defines item data.
+- If a prefab file has item data defined, it represents an inventory item only if the item name has a valid translation.
+
--- ValheimItemDataExport/ValheimItemDataExtract/Program.cs
+++ ValheimItemDataExport/ValheimItemDataExtract/Program.cs
@@ -1,93 +1,214 @@
 ﻿using CsvHelper;
+using System.Globalization;
 
-// Print help if asked for
-if (args.Length > 0 && new string[] { "/?", "--help" }.Any(s => args[0].ToLower().Equals(s)))
+// Command line arguments and switches
+string[] SwitchHelp = ["/?", "/h", "--help"];
+string[] SwitchVerbose = ["/v", "--verbose"];
+string[] SwitchLocalization = ["/l", "--localization"];
+bool SwitchEnabled(string[] sw) => args.Length > 0 && args.Any(a => sw.Any(s => a.ToLower().Equals(s)));
+
+// Check switches that affect program behaviour
+var VerboseMode = true;//SwitchEnabled(SwitchVerbose);
+if (SwitchEnabled(SwitchHelp))
 {
-    Console.WriteLine($"Usage: ValheimItemDataExtract [ASSETROOT] [CSVFILE]");
-    Console.WriteLine($"Extract Valheim item data from unpacked Unity assets within ASSETROOT directory and write them to CSVFILE.");
-    Environment.Exit(0);
+    ShowHelpThenExit();
 }
 
-// Prepare what we need in order to run, including using defaults if cmdline args are missing
-var PathRoot = args.Length > 0 ? args[0] : @"c:\_temp\ValheimExports\AssetRipper\globalgamemanagers\ExportedProject\";
-var PathFile = args.Length > 1 ? args[1] : @".\SharedItemData.csv";
+// Setup paths and data containers for data extraction
+var PathSource = Path.GetFullPath(args.Length > 0 ? args[0] :  @".\");
+var PathDestination = args.Length > 1 ? args[1] : "SharedItemData.csv";
 var Localization = new Dictionary<string, string>();
-var ItemData = new List<Object>();
+var ItemData = new List<object>();
 
-// Process assets and save result
-Console.WriteLine($"ASSETROOT is {Path.GetFullPath(PathRoot)}.");
-LoadLocalization(PathRoot);
-ExtractItemData(PathRoot);
-SaveItemDataAsCsvFile(PathFile);
+// Data extration steps
+Inform($"Asset root is '{PathSource}'.", isVerbose: true);
+Inform("Extracting item data...");
+EnsureDir(PathSource);
+LoadLocalization();
+ExtractItemData();
+SaveItemData();
+Inform("Item data extracted successfully");
 
-// Function to load localization file into a dictionary with name as key and English translation as value
-void LoadLocalization(string pathRoot)
+// Print information message as verbose or normal
+void Inform(string message, bool isVerbose = false)
 {
-    var subPath = @"Assets\Resources\localization.txt";
-    Console.WriteLine($"Processing file {subPath}...");
-
-    foreach (var line in File.ReadAllText($@"{pathRoot}\{subPath}").Split("\"\n\""))
+    if (!VerboseMode && isVerbose)
     {
-        var parts = line.Split(new string[] { "\",\"", ",\"" }, StringSplitOptions.RemoveEmptyEntries);
-        if (parts.Length > 1 && !Localization.ContainsKey(parts[0]))
-        {
-            Localization[parts[0]] = parts[1];
-        }
+        return;
     }
-    Console.WriteLine($"Loaded {Localization.Count} translations.");    
+    Console.WriteLine(message);
 }
 
-// Function to extract item data from prefab files that have it and store it in a list
-void ExtractItemData(string pathRoot)
-{
-    string[] ids = { "m_name", "m_teleportable", "m_useDurability", "m_maxDurability", "m_durabilityPerLevel", "m_maxStackSize", "m_maxQuality", "m_itemType" };
-    var subPath = @"Assets\PrefabInstance";
-    var prefabs = Directory.GetFiles($@"{pathRoot}/{subPath}", "*.prefab", SearchOption.TopDirectoryOnly);
-    Console.WriteLine($"Processing {prefabs.Length} files in directory {subPath}...");
+// Show help message and then exit program gracefully
+void ShowHelpThenExit() => 
+    Exit("Extracts Valheim inventory item data from asset files.\n\n" +
+        $"{AppDomain.CurrentDomain.FriendlyName} source destination [{SwitchVerbose[0]}] [{SwitchLocalization[0]}]\n\n" +
+        $"  {"source",-12} Specifies the path to the asset root directory.\n" +
+        $"  {"destination",-12} Specifies the path to the directory to export to.\n" +
+        $"  {SwitchVerbose[0],-12} Verbose mode: print additional info\n" +
+        $"  {SwitchLocalization[0],-12} Dump extracted localization data to file\n" +
+        $"\nAsset files has to be extracted from the game beforehand, using some another tool.\n" +
+        $"Tool that has been verified to work for this is AssetRipper.\n\n", 0);
 
-    foreach (var filename in prefabs)
+// Exit program prematurely after printing reason (error message if exit code represents an error, ie is non zero)
+void Exit(string reason, int exitCode)
+{
+    Inform(exitCode != 0 ? $"ERROR: {reason}\nExtraction aborted!" : reason);
+    Environment.Exit(exitCode);
+}
+
+// Verify that a directory exists/is accessible and if not exit program with immediately with error
+void EnsureDir(string path)
+{
+    if (!Directory.Exists(path))
     {
+        Exit($"Directory does not exist or is inaccessible: '{path}", 2);
+    }
+}
+
+// Verify that a file exists/is accessible and if not exit program with immediately with error
+void EnsureFile(string path)
+{
+    if (!File.Exists(path))
+    {
+        Exit($"File does not exist or is inaccessible: '{path}'", 1);
+    }
+}
+
+// Load English localization from main localization file into dictionary with names as keys and corresponding translations as values
+void LoadLocalization()
+{
+    var sourceFile = Path.Combine("Resources", "localization.txt");
+    var fullPathSourceFile = Path.Combine(PathSource, sourceFile);
+
+    Inform($"Processing localization file '{(VerboseMode ? fullPathSourceFile : sourceFile)}'...");
+    EnsureFile(fullPathSourceFile);
+
+    // Seems localization file is a CSV (but with plenty of duplicates and empty values)
+    using (var reader = new StreamReader(fullPathSourceFile))
+    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+    {
+        while (csv.Read())
+        {
+            var key = csv[0];
+            var value = csv[1];
+            if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(value) || Localization.ContainsKey(key))
+            {
+                continue;
+            }
+            Localization.Add(key, value);
+        }
+    } 
+    Inform($"Loaded {Localization.Count} translations.");   
+    
+    if (SwitchEnabled(SwitchLocalization))
+    {
+        var destinationFile = "LocalizationData.csv";
+        var fullPathDestinationFile = Path.Combine(PathDestination, destinationFile);
+        SaveDataAsCsvFile(Localization.Select(kvp => new {Name=kvp.Key, Text=kvp.Value}), fullPathDestinationFile);
+        Inform($"Localization data saved to '{destinationFile}'");
+    }
+}
+
+// Extract items from all prefab files that have item data defined, but add only those that has a translation
+void ExtractItemData()
+{
+    string[] keys = ["m_name", "m_teleportable", "m_useDurability", "m_maxDurability", "m_durabilityPerLevel", "m_maxStackSize", "m_maxQuality", "m_itemType", "m_worldLevel", "m_pickedUp"];
+    var filePattern = "*.prefab";
+    var subDir = "PrefabInstance";
+    var relPath = Path.Combine(subDir, filePattern);
+    var fullPath = Path.Combine(PathSource, subDir);
+
+    Inform($"Processing prefab files '{relPath}'...");
+    EnsureDir(fullPath);
+
+    var prefabs = Directory.GetFiles(fullPath, filePattern, SearchOption.TopDirectoryOnly);
+    if (prefabs.Length == 0)
+    {
+        Exit($"No prefab files ({filePattern}) found in: '{fullPath}'", 3);
+    }
+
+    char[] valueTrimChars = [' ', '\t', '$']; // Note the $ since that's preceding localized strings
+    var discardedCount = 0;
+    var ignoredCount = 0;
+    Inform($"Processing {prefabs.Length} prefab files", isVerbose: true);
+
+    for (int i = 0; i < prefabs.Length; i++)
+    {
+        var filename = prefabs[i];
+        var itemId = Path.GetFileNameWithoutExtension(filename);
         var sections = File.ReadAllText(filename).Split("m_itemData:\n");
         if (sections.Length < 2)
         {
+            // No item data in this prefab file means not an inventory item at all. Skip to next
+            ignoredCount++;
+            Inform($"{"",2}[{i+1,4}] {"Ignored",9}: {itemId}", isVerbose: true);
             continue;
         }
-        var data = new Dictionary<string, string>(ids.Length);
-        foreach (var line in sections[1].Split("\n").Where(line => line.Contains(':') && ids.Any(id => line.Contains($"{id}:"))))
+
+        // Extract data for this item
+        var data = new Dictionary<string, string>(keys.Length);
+        foreach (var line in sections[1].Split("\n").Where(line => line.Contains(':') && keys.Any(id => line.Contains($"{id}:"))))
         {
-            var parts = line.Split(':', 2); // Some variable values are strings with : in them, we don't want to split there
-            var id = parts[0].Trim();
-            var value = parts[1].Trim(new char[] { ' ', '\t', '$' }); // Note the $ since that's preceeding localized string m_name
-            data[id] = value;
+            var parts = line.Split(':', 2); // Some values are strings with ':' in them. We don't want to split them, take first split only
+            var key = parts[0].Trim();
+            var value = parts[1].Trim(valueTrimChars); 
+            data[key] = value;
         }
-        if (Localization.TryGetValue(data[ids[0]], out string? englishTranslation))
+
+        // "name" is used specifically for localization references, but "id" is what we export as name
+        var itemName = data[keys[0]];
+
+        // Only add to extracted items if the item has a valid translation 
+        if (Localization.TryGetValue(itemName, out string? itemText))
         {
             // Anonymous type objects will do as records
-            ItemData.Add(new 
+            var id = new 
             {
-                ItemName = Path.GetFileNameWithoutExtension(filename),
-                IsTeleportable = Convert.ToInt32(data[ids[1]]) != 0,
-                UsesDurability = Convert.ToInt32(data[ids[2]]) != 0,
-                MaxDurability = Convert.ToInt32(data[ids[3]]),
-                DurabilityPerLevel = Convert.ToInt32(data[ids[4]]),
-                MaxStack = Convert.ToInt32(data[ids[5]]),
-                DisplayName = englishTranslation,
-                MaxQuality = Convert.ToInt32(data[ids[6]]),
-                ItemType = Convert.ToInt32(data[ids[7]]),
-            });
+                ItemName = itemId.EndsWith("_0") ? itemId[..^2] : itemId, // Fix for prefab file suffix introduced in 0.218.15 (Ashlands)
+                IsTeleportable = Convert.ToInt32(data[keys[1]]) != 0,
+                UsesDurability = Convert.ToInt32(data[keys[2]]) != 0,
+                MaxDurability = Convert.ToInt32(data[keys[3]]),
+                DurabilityPerLevel = Convert.ToInt32(data[keys[4]]),
+                MaxStack = Convert.ToInt32(data[keys[5]]),
+                DisplayName = itemText,
+                MaxQuality = Convert.ToInt32(data[keys[6]]),
+                ItemType = Convert.ToInt32(data[keys[7]]),
+            };
+            var itemIdFixed = id.ItemName != itemId ? $"{itemId} -> {id.ItemName}" : itemId;
+            ItemData.Add(id);
+            Inform($"{"",2}[{i+1,4}] {"Added",9}: {itemIdFixed} ({itemText})", isVerbose:true);            
+        }
+        else
+        {
+            // Not an inventory item after all. Discard but count for information purpose.
+            discardedCount++;
+            Inform($"{"",2}[{i+1,4}] {"Discarded",9}: {itemId} ({itemName})", isVerbose:true);
         }
     }
-    Console.WriteLine($"Extracted {ItemData.Count} items.");    
+    Inform($"{prefabs.Length} prefab files processed{(VerboseMode ? $": {ignoredCount} ignored, {discardedCount} discarded, {ItemData.Count} used" : "")}.");
 }
 
-
-// Function to save the extracted item data to CSV file, provided out of the box by CSV helper package
-void SaveItemDataAsCsvFile(string pathFile)
+void SaveDataAsCsvFile(IEnumerable<object> data, string filepath)
 {
-    using (var writer = new StreamWriter(pathFile))
-    using (var csv = new CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+    try
     {
-        csv.WriteRecords(ItemData);
+        using (var writer = new StreamWriter(filepath))
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.WriteRecords(data);            
+        }
+        Inform($"Saved {data.Count()} items to '{filepath}'.");
     }
-    Console.WriteLine($"Saved {ItemData.Count} items to {Path.GetFullPath(pathFile)}");
+    catch (Exception ex)
+    {
+        Exit($"Failed to save file '{filepath}'. {ex.Message}", 3);
+    }
+}
+
+// Save all extracted items and their data to a CSV file
+void SaveItemData()
+{
+    SaveDataAsCsvFile(ItemData, PathDestination);
+
 }
